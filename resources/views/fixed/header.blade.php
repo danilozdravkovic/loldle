823ab084b5575876e4dca6b4a737adb32ee6e2f6
danilozdravkovic <danilo.zdravkovic01.biz@gmail.com>
2023-05-31 22:08:25 +0200

    deleteAll

--- resources/views/fixed/header.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="row">
-    <a href="{{route('home')}}"><img src="{{asset('assets/img/logo.png')}}" alt="logo"/></a>
-</div>
--- storage/framework/views/3bcc72e3650e9cb0d4fe108bcaab4523df36ebe0.php
+++
@@ -1,4 +0,0 @@
-<div class="row">
-    <a href="<?php echo e(route('home')); ?>"><img src="<?php echo e(asset('assets/img/logo.png')); ?>" alt="logo"/></a>
-</div>
-<?php /**PATH D:\ICT\III godina\Web\Web programiranje PHP 2\loldle\resources\views/fixed/header.blade.php ENDPATH**/ ?>
